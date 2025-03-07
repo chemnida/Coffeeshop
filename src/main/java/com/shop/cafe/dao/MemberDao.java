e9e85f704335c9a0d0afc6ede042e59130aa7465
chemnida <chaemin9073@gmail.com>
2025-03-07 16:14:44 +0900

    feat:login 완료

--- src/main/java/com/shop/cafe/dao/MemberDao.java
+++ src/main/java/com/shop/cafe/dao/MemberDao.java
@@ -3,6 +3,7 @@ package com.shop.cafe.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -25,16 +26,36 @@ public class MemberDao {
 	@Value("${spring.datasource.password}")
 	private String DB_PW;
 	
+	public Member login(Member m) throws Exception {
+		System.out.println("MemberDao login()호출됨");
+		
+		Class.forName(DB_DRIVER);
+		Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PW);	
+		
+		PreparedStatement stmt = con.prepareStatement("select * from member where email=? and pwd=?");
+		
+		stmt.setString(1, m.getEmail());
+		stmt.setString(2, m.getPwd());
+		
+		ResultSet rs = stmt.executeQuery("select * from where email='"+m.getEmail()+ "' and pwd ='"+m.getPwd()+"' ");
+		if(rs.next()) {
+			String nickname = rs.getString("nickname");
+			m.setNickname(nickname);
+		}
+		return m;
+	
+	}
+	
 	public void insertMember(Member m) throws Exception {
 		Class.forName(DB_DRIVER);
-			Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PW);	
-			PreparedStatement stmt=con.prepareStatement("insert into member(email, pwd, nickname) values(?,?,?)");
+		Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PW);	
+		PreparedStatement stmt=con.prepareStatement("insert into member(email, pwd, nickname) values(?,?,?)");
 
-			stmt.setString(1, m.getEmail());
-			stmt.setString(2, m.getPwd());
-			stmt.setString(3, m.getNickname());
-			int i=stmt.executeUpdate();
-			System.out.println(i+"행이 insert되었습니다");
+		stmt.setString(1, m.getEmail());
+		stmt.setString(2, m.getPwd());
+		stmt.setString(3, m.getNickname());
+		int i=stmt.executeUpdate();
+		System.out.println(i+"행이 insert되었습니다");
 	}
 
 }
